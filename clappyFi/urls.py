4627e83752030623ad01990b13cdf06a0f94b8b1
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-07-11 21:19:09 +0530

    add: referal
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- clappyFi/urls.py
+++ clappyFi/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from clappyFi.views import home,Login,UserPage
+from clappyFi.views import home,Login,UserPage,Refer
 
 urlpatterns = [
     path('login/',Login,name='login'),
     path('',home,name='home'),
-    path('user/',UserPage, name='userpage')
+    path('user/',UserPage, name='userpage'),
+    path('user/refer/<str:zzz>/',Refer,name='Refer')
 
 
 ]
--- clappyFi/views.py
+++ clappyFi/views.py
@@ -1,3 +1,4 @@
+from dataclasses import replace
 from django.shortcuts import render,redirect
 import requests
 import os
@@ -21,30 +22,48 @@ def Login(request):
         return redirect('home')
 
 
+def cleandata(xx):
+    temp=""
+    for i in xx:
+        if i.isnumeric():
+            break
+    
+        temp+=i
+    
+    temp=temp.replace(' ','_')
+
+    return temp
+
 
 def UserPage(request):
     WalletAddress = request.session['WalletAddress']
-    all_unique_titles = set()
-    converter = lambda x: x.replace(' ', '_')
+    all_unique_titles = []
+    # converter = lambda x: x.replace(' ', '_')
 
 
-    baseURL = 'https://eth-mainnet.g.alchemy.com/v2/<alchemy_api_key>'
+    baseURL = 'https://eth-mainnet.g.alchemy.com/v2/<API_KEY>'
     url = baseURL+'/getNFTs/?owner=0x8a502e0e3EDa70eAE505a6Fa0FA49eB29b85fe5B'
     x = requests.get(url)
     x = x.json()
     print('xxxxxxxxxxxxxxxxxxxxxx')
     # print(x)
-    for i in x['ownedNfts']:
-        all_unique_titles.add(i['title'])
-
 
     with open('personal.json', 'w') as json_file:
         json.dump(x, json_file)
+
+    for i in x['ownedNfts']:
+        try:
+            all_unique_titles.append([cleandata(i['title']),i['metadata']['image']])
+        except:
+            print('format wrong')
+    
         
     print(all_unique_titles)
-    my_list = list(map(converter, all_unique_titles))
+ 
+    return render(request,'userpage.html',{'WalletAddress':WalletAddress,'all_unique_titles':all_unique_titles})
 
 
-    
-    return render(request,'userpage.html',{'WalletAddress':WalletAddress,'all_unique_titles':my_list})
+def Refer(request,zzz):
 
+
+    return render(request,'refer.html',{'zzz':zzz})
