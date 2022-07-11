e96ea45b6333e715bcaa8aa4b13b211b750873d7
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-07-11 20:02:26 +0530

    fetch nfts by owner
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- clappyFi/views.py
+++ clappyFi/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render,redirect
+import requests
+import os
+import json
 
 # Create your views here.
 def home(request):
@@ -17,9 +20,31 @@ def Login(request):
     else:
         return redirect('home')
 
+
+
 def UserPage(request):
     WalletAddress = request.session['WalletAddress']
+    all_unique_titles = set()
+    converter = lambda x: x.replace(' ', '_')
+
+
+    baseURL = 'https://eth-mainnet.g.alchemy.com/v2/<alchemy_api_key>'
+    url = baseURL+'/getNFTs/?owner=0x8a502e0e3EDa70eAE505a6Fa0FA49eB29b85fe5B'
+    x = requests.get(url)
+    x = x.json()
+    print('xxxxxxxxxxxxxxxxxxxxxx')
+    # print(x)
+    for i in x['ownedNfts']:
+        all_unique_titles.add(i['title'])
+
+
+    with open('personal.json', 'w') as json_file:
+        json.dump(x, json_file)
+        
+    print(all_unique_titles)
+    my_list = list(map(converter, all_unique_titles))
+
+
     
-    
-    return render(request,'userpage.html',{'WalletAddress':WalletAddress})
+    return render(request,'userpage.html',{'WalletAddress':WalletAddress,'all_unique_titles':my_list})
 
